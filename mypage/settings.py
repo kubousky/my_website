2d07f59ed1d9562e6e7a111c12d41d351e334cae
kubousky <kparcheta@gmail.com>
2019-12-05 16:23:32 +0100

    herokuu

--- mypage/settings.py
+++ mypage/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -57,7 +58,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-#'whitenoise.middleware.WhiteNoiseMiddleware',
+#
 
 ROOT_URLCONF = 'mypage.urls'
 
@@ -131,7 +132,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'portfolio/static/')
 STATIC_URL = '/static/'
 
 
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 #django_heroku.settings(locals())
