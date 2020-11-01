19203ae0bff07fa3bafe46fcb4039f84b44f7683
kubousky <kparcheta@gmail.com>
2020-11-01 23:42:08 +0100

    from sqlite3 to postgres

--- mypage/settings.py
+++ mypage/settings.py
@@ -86,8 +86,12 @@ WSGI_APPLICATION = 'mypage.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django_personal_web', 
+        'USER': 'postgres', 
+        'PASSWORD': 'Ryszard1',
+        'HOST': '127.0.0.1', 
+        'PORT': '5432',
     }
 }
 
